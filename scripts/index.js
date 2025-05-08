72c41cc875f6f9ee59600c9b2c4d314479744470
akshayk423 <akshay.khalasi3@gmail.com>
2025-05-08 01:26:52 -0700

    UX functionality for modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,31 @@
 const editProfileBtn = document.querySelector(".profile__edit-profile-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+
+// get profile name and description
+const profileInformation = document.querySelector(".profile__column");
+const profileName = profileInformation.querySelector(".profile__name");
+const profileDescription = profileInformation.querySelector(
+  ".profile__description"
+);
+
+// get input elements from the modals
+const profileNameInput = editProfileModal.querySelector("#profile-name-input");
+const profileDescriptionInput =
+  editProfileModal.querySelector("#describe-input");
+
+const imageLinkInput = newPostModal.querySelector("#image-link-input");
+const captionInput = newPostModal.querySelector("#caption-input");
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  fillEditProfileInputs();
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
@@ -19,4 +38,22 @@ newPostBtn.addEventListener("click", function () {
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
+});
+
+editProfileForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+});
+
+function fillEditProfileInputs() {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+}
+
+newPostForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  console.log(
+    `Image Link: ${imageLinkInput.value} Caption: ${captionInput.value}`
+  );
 });
