0be96a12d6b42344773a9d58ec6f56ea1bd37867
akshayk423 <akshay.khalasi3@gmail.com>
2025-05-13 17:00:38 -0700

    added open and close modal functions

--- scripts/index.js
+++ scripts/index.js
@@ -50,28 +50,36 @@ const profileDescriptionInput =
 const imageLinkInput = newPostModal.querySelector("#image-link-input");
 const captionInput = newPostModal.querySelector("#caption-input");
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
   fillEditProfileInputs();
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 editProfileForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   profileName.textContent = profileNameInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 function fillEditProfileInputs() {
@@ -84,7 +92,7 @@ newPostForm.addEventListener("submit", (evt) => {
   console.log(
     `Image Link: ${imageLinkInput.value} Caption: ${captionInput.value}`
   );
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 initialCards.forEach(function (element) {
